35907fdbf1b9572e9f9aa708584b2c2040291478
Luis Miguel Sanchez Pinilla <luism.sanchezp@autonoma.edu.co>
2022-10-10 14:04:27 -0400

    Add all controllers routes to api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,4 @@
 <?php
-namespace App\Http\Controllers\api\v1;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,6 +14,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//User routes
+Route::get('v1/users',
+    [App\Http\Controllers\api\v1\UserController::class,'index'])->name('users.index');
+Route::get('v1/users/{user}',
+    [App\Http\Controllers\api\v1\UserController::class,'show'])->name('users.show');
+Route::post('v1/users',
+    [App\Http\Controllers\api\v1\UserController::class,'store'])->name('users.store');
+
+//ParkingLot routes
+Route::get('v1/parkinglots',
+    [App\Http\Controllers\api\v1\ParkingLotController::class,'index'])->name('parkinglots.index');
+Route::get('v1/parkinglots/{parkinglot}',
+    [App\Http\Controllers\api\v1\ParkingLotController::class,'show'])->name('parkinglots.show');
+
+//VehicleType routes
+Route::get('v1/vehicletypes',
+    [App\Http\Controllers\api\v1\VehicleTypeController::class,'index'])->name('vehicletypes.index');
+Route::get('v1/vehicletypes/{vehicletype}',
+    [App\Http\Controllers\api\v1\VehicleTypeController::class,'show'])->name('vehicletypes.show');
+
 Route::post('/v1/login',
     [App\Http\Controllers\api\v1\AuthController::class,
         'login'])->name('api.login');
@@ -23,4 +42,64 @@ Route::middleware(['auth:sanctum'])->group(function() {
     Route::post('/v1/logout',
         [App\Http\Controllers\api\v1\AuthController::class,
             'logout'])->name('api.logout');
+
+    //User routes
+    Route::put('v1/users/{user}',
+        [App\Http\Controllers\api\v1\UserController::class,'update'])->name('users.update');
+
+    //Person routes
+    Route::get('v1/persons',
+        [App\Http\Controllers\api\v1\PersonController::class,'index'])->name('persons.index');
+    Route::get('v1/persons/{person}',
+        [App\Http\Controllers\api\v1\PersonController::class,'show'])->name('persons.show');
+    Route::post('v1/persons',
+        [App\Http\Controllers\api\v1\PersonController::class,'store'])->name('persons.store');
+    Route::put('v1/persons/{person}',
+        [App\Http\Controllers\api\v1\PersonController::class,'update'])->name('persons.update');
+
+    //ParkingLot routes
+    Route::post('v1/parkinglots',
+        [App\Http\Controllers\api\v1\ParkingLotController::class,'store'])->name('parkinglots.store');
+    Route::put('v1/parkinglots/{parkinglot}',
+        [App\Http\Controllers\api\v1\ParkingLotController::class,'update'])->name('parkinglots.update');
+
+    //Parking Spot routes
+    Route::get('v1/parkingspots',
+        [App\Http\Controllers\api\v1\ParkingSpotController::class,'index'])->name('parkingspots.index');
+    Route::get('v1/parkingspots/{parkingspot}',
+        [App\Http\Controllers\api\v1\ParkingSpotController::class,'show'])->name('parkingspots.show');
+
+    //Ticket routes
+    Route::get('v1/tickets',
+        [App\Http\Controllers\api\v1\TicketController::class,'index'])->name('tickets.index');
+    Route::get('v1/tickets/{ticket}',
+        [App\Http\Controllers\api\v1\TicketController::class,'show'])->name('tickets.show');
+    Route::post('v1/tickets',
+        [App\Http\Controllers\api\v1\TicketController::class,'store'])->name('tickets.store');
+    Route::put('v1/tickets/{ticket}',
+        [App\Http\Controllers\api\v1\TicketController::class,'update'])->name('tickets.update');
+
+    //Vehicle routes
+    Route::get('v1/vehicles',
+        [App\Http\Controllers\api\v1\VehicleController::class,'index'])->name('vehicles.index');
+    Route::get('v1/vehicles/{vehicle}',
+        [App\Http\Controllers\api\v1\VehicleController::class,'show'])->name('vehicles.show');
+    Route::post('v1/vehicles',
+        [App\Http\Controllers\api\v1\VehicleController::class,'store'])->name('vehicles.store');
+    Route::put('v1/vehicles/{vehicle}',
+        [App\Http\Controllers\api\v1\VehicleController::class,'update'])->name('vehicles.update');
+
+    //VehicleType routes
+    Route::post('v1/vehicletypes',
+        [App\Http\Controllers\api\v1\VehicleTypeController::class,'store'])->name('vehicletypes.store');
+
+    //Cashier routes
+    Route::get('v1/cashiers',
+        [App\Http\Controllers\api\v1\CashierController::class,'index'])->name('cashiers.index');
+    Route::get('v1/cashiers/{cashier}',
+        [App\Http\Controllers\api\v1\CashierController::class,'show'])->name('cashiers.show');
+    Route::post('v1/cashiers',
+        [App\Http\Controllers\api\v1\CashierController::class,'store'])->name('cashiers.store');
+    Route::put('v1/cashiers/{cashier}',
+        [App\Http\Controllers\api\v1\CashierController::class,'update'])->name('cashiers.update');
 });
